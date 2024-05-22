4eaa915b260acb280f8f4e800785b69cdc18b1d9
colorfulvoid <colorfulvoid@keemail.me>
2024-05-22 19:28:12 +0100

    fix: fix hardhat compilation issue

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,15 +1,10 @@
 require("@nomicfoundation/hardhat-toolbox");
-require("@nomiclabs/hardhat-ethers");
 
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
   defaultNetwork: "hardhat",
   networks: {
     hardhat: {},
-    myQuickNode: {
-      url: process.env.quickNodeKey,
-      accounts: [process.env.privateKey],    
-    },
   },
   solidity: {
     version: "0.8.24",
