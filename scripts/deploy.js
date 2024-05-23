83d6d0b6e63d1c3a1ff23f5187c9cf6e92ae8e36
0xodus <samuelmsuu@gmail.com>
2024-05-23 19:30:02 +0300

    feat: Update npm dependencies and hardhat configuration
    This commit updates the npm dependencies by adding the "@nomiclabs/hardhat-ethers" package and the hardhat configuration to include the necessary plugin. It also includes previous commits that added Chainlink integration, a new field to the Donation struct, and fixed bugs related to performUpkeep and donate functions.

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -1,16 +1,46 @@
-// This script is used to deploy the Chainrity contract to the local blockchain
-const { ethers } = require("hardhat");
+// // This script is used to deploy the Chainrity contract to the local blockchain
+// const { ethers } = require("hardhat");
+
+// async function main() {
+//     // Get the Chainrity contract to deploy
+//     const Chainrity = await ethers.getContractFactory("chainrity");
+
+//     // Start deployment
+//     const chainrity = await Chainrity.deploy();
+//     await chainrity.deploy();
+
+//     console.log("Chainrity deployed to:", chainrity.address);
+//     console.log("Transaction hash:", chainrity.deployTransaction.hash);
+// }
+
+// main()
+//     .then(() => process.exit(0))
+//     .catch((error) => {
+//         console.error(error);
+//         process.exit(1);
+//     });
+
+// This script is used to deploy the DonationPlatform contract to the local blockchain
+const BigNumber = require('ethers').BigNumber;
+const minimumDonationInEther = 0.1;
+
 
 async function main() {
-    // Get the Chainrity contract to deploy
-    const Chainrity = await ethers.getContractFactory("Chainrity");
+    // Get the DonationPlatform contract to deploy
+    const DonationPlatform = await ethers.getContractFactory("DonationPlatform");
+   
 
-    // Start deployment
-    const chainrity = await Chainrity.deploy();
-    await chainrity.deployed();
 
-    console.log("Chainrity deployed to:", chainrity.address);
-    console.log("Transaction hash:", chainrity.deployTransaction.hash);
+    const priceFeedAddress = "0x5f4eC3Df9cbd43714FE2740f5E3616155c5b8419"; // replace with your price feed address
+    const minimumDonation = hre.ethers.utils.parseEther(minimumDonationInEther.toString());
+    ; // 0.1 ETH
+
+    // Start deployment, replace with your constructor arguments if any
+    const donationPlatform = await DonationPlatform.deploy(priceFeedAddress, minimumDonation);
+    await donationPlatform.deployed();
+
+    console.log("DonationPlatform deployed to:", donationPlatform.address);
+    console.log("Transaction hash:", donationPlatform.deployTransaction.hash);
 }
 
 main()
